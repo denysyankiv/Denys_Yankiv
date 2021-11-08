cb7c85e2407ee28089358d696dea66b2e523f3e8
Denys Yankiv <denys.yankiv@gmail.com>
2021-11-08 20:41:44 +0200

    add part 1-5

--- src/main/java/com/epam/spring/homework1/Main.java
+++ src/main/java/com/epam/spring/homework1/Main.java
@@ -1,8 +1,14 @@
 package com.epam.spring.homework1;
 
+import com.epam.spring.homework1.config.BeansConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
   public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
+    // Part 1 - 5
 
   }
 }
---
+++ src/main/java/com/epam/spring/homework1/config/OtherConfig.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.epam.spring.homework1.other")
+public class OtherConfig {
+
+}
